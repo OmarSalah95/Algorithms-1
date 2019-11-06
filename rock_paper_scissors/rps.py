f11be84449a63bb49993fb80126fc880fd81a863
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-11-06 15:16:23 -0800

    Managed to figure out both the Cookie monster challenge and the Rock Paper Sciscors algorithms

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  def recursion_rps(possibilities):
+    updated_possibilities = []
+    for start in [['rock'], ['paper'], ['scissors']]:
+      for rest in possibilities:
+        updated_possibilities.append(start+rest)
+    return updated_possibilities
+  
+  
+  if n == 0:
+    return [[]]
+  possibilities = [['rock'], ['paper'], ['scissors']]
+  while n > 1:
+    possibilities = recursion_rps(possibilities)
+    n -= 1
+  return possibilities
+
 
 
 if __name__ == "__main__":
